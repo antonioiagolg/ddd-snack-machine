654ee5f75f149603eea7f28ec7a85c2d356a7998
Antonio Iago <antoniogaspar@seap.ma.gov.br>
2019-05-30 17:38:33 -0300

    Initial commit

---
+++ src/main/java/ddd/logic/LogicApplication.java
@@ -0,0 +1,13 @@
+package ddd.logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ddd/logic/LogicApplicationTests.java
@@ -0,0 +1,16 @@
+package ddd.logic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
